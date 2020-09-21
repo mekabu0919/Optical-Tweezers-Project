50229bd736bb8fb9c9d1f6f92be8d12a0c4a20e4
mekabu0919 <mekabu0919@gmail.com>
2020-09-21 18:46:28 +0900

    frame number into metafile

--- FilePartial.py
+++ FilePartial.py
@@ -1,15 +1,42 @@
 import mmap
+import ctypes as ct
+import numpy as np
+import os
+import socket
+import matplotlib.pyplot as plt
 
+if os.path.dirname(__file__):
+    os.chdir(os.path.dirname(__file__))
+
+simMode = True
+if socket.gethostname() == "EXPERIMENT2":
+    simMode = False
+    os.chdir(r'../Andor_dll/Andor_dll')
+else:
+    os.chdir(r'Andor_dll\Andor_dll')
+
+dll = ct.windll.LoadLibrary(r'..\x64\Release\Andor_dll.dll')
+dll.InitialiseUtilityLibrary()
 dir = r"C:\Users\Ryoma\Documents\00_Simulation\program_test\23_41_58"
 metafile = dir + "/metaSpool.txt"
 file = dir + "/movie.dat"
+dll.convertBuffer.argtypes = [ct.POINTER(ct.c_ubyte), ct.POINTER(ct.c_ushort), ct.c_longlong, ct.c_longlong, ct.c_longlong]
 with open(metafile, mode='r') as f:
     metadata = f.readlines()
 
 size, code, stride, height, width, frate = metadata
+size, stride, height, width, frate = map(int, [size, stride, height, width, frate])
 
 with open(file, mode='r+b') as f:
     mm = mmap.mmap(f.fileno(), 0)
+    mm.seek(size*2)
     img = mm.read(int(size))
-    print(img)
+    buffer = ct.cast(img, ct.POINTER(ct.c_ubyte))
+    outputBuffer = (ct.c_ushort * (width * height))()
+    ret = dll.convertBuffer(buffer, outputBuffer, width, height, stride)
+    outimg = np.array(outputBuffer).reshape(height, width)
+    plt.imshow(outimg)
+    plt.show()
     mm.close()
+
+dll.FinaliseUtilityLibrary()
